0eb06f51fb9225b193f6ed49d5a16d6740c822aa
enty3 <104058678+enty3@users.noreply.github.com>
2023-06-16 08:51:16 +0200

    update

--- src/components/TwitchEmbedVideo.tsx
+++ src/components/TwitchEmbedVideo.tsx
@@ -4,12 +4,8 @@ import ReactTwitchEmbedVideo from 'react-twitch-embed-video';
 const MyTwitchStream = () => {
     return (
         <div className="twitch-stream-container">
-            <ReactTwitchEmbedVideo
-                channel="educup2"
-                layout="video"
-                height={250}
-                width={800}
-            />
+            <ReactTwitchEmbedVideo channel="educup2" layout={"video"} height={250} width={800}/>
+
         </div>
     );
 };
