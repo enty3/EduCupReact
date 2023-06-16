822dca865ca5bb17a4e13a2c993eeacc565201a0
enty3 <104058678+enty3@users.noreply.github.com>
2023-06-16 09:10:02 +0200

    update

--- src/components/Coments.tsx
+++ src/components/Coments.tsx
@@ -1,47 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const Komentare = () => {
-    const [komentare, setKomentare] = useState([]);
-    const [novyKomentar, setNovyKomentar] = useState('');
+  const [komentare, setKomentare] = useState<{ id: number; text: string }[]>([])
+  const [novyKomentar, setNovyKomentar] = useState('')
 
-    const handleInputChange = (event) => {
-        setNovyKomentar(event.target.value);
-    };
+  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setNovyKomentar(event.target.value)
+  }
 
-    const handleFormSubmit = (event) => {
-        event.preventDefault();
-        if (novyKomentar.trim() !== '') {
-            const novyKomentarObjekt = {
-                id: Date.now(),
-                text: novyKomentar
-            };
-            setKomentare([...komentare, novyKomentarObjekt]);
-            setNovyKomentar('');
-        }
-    };
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    if (novyKomentar.trim() !== '') {
+      const novyKomentarObjekt = {
+        id: Date.now(),
+        text: novyKomentar,
+      }
+      setKomentare([...komentare, novyKomentarObjekt])
+      setNovyKomentar('')
+    }
+  }
 
-    return (
-        <div>
-            <h2>Komentáře</h2>
-            <ul>
-                {komentare.length === 0 ? (
-                    <li>Žádné komentáře</li>
-                ) : (
-                    komentare.map((komentar) => (
-                        <li key={komentar.id}>{komentar.text}</li>
-                    ))
-                )}
-            </ul>
-            <form onSubmit={handleFormSubmit}>
+  return (
+    <div>
+      <h2>Komentáře</h2>
+      <ul>
+        {komentare.length === 0 ? (
+          <li>Žádné komentáře</li>
+        ) : (
+          komentare.map((komentar) => (
+            <li key={komentar.id}>{komentar.text}</li>
+          ))
+        )}
+      </ul>
+      <form onSubmit={handleFormSubmit}>
         <textarea
-            value={novyKomentar}
-            onChange={handleInputChange}
-            placeholder="Napište komentář..."
+          value={novyKomentar}
+          onChange={handleInputChange}
+          placeholder="Napište komentář..."
         />
-                <button type="submit">Přidat komentář</button>
-            </form>
-        </div>
-    );
-};
+        <button type="submit">Přidat komentář</button>
+      </form>
+    </div>
+  )
+}
 
-export default Komentare;
+export default Komentare
